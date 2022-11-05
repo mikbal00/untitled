9ca3a13a534e428ce2b4877372538d7b3e967143
PC <m.ikbaldogancay@hotmail.com>
2022-11-05 15:53:10 +0300

    first

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Ben M.Ikbal Cok Yakısıklıyım");
+
+
+    }
+
+
+}
